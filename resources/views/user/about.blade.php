70b99085f01cb9f6cba1a0cac7e0a15a44a2c1ae
Yoza prawama <101876725+prawamayoza@users.noreply.github.com>
2023-10-11 09:18:40 +0700

    logo abaout

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -48,7 +48,7 @@
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img class="img-fluid position-absolute w-200 h-200" src="{{ asset('logo.jpeg') }}" alt=""
+                        <img class="img-fluid position-absolute w-200 h-200" src="{{ asset('logo-sma.png') }}" alt=""
                             style="object-fit: cover;">
                     </div>
                 </div>
